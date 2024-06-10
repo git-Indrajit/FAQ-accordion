1072a457e3abcae6b0701441fbbad3a2172c1dbf
Indrajitgithub <126161834+git-Indrajit@users.noreply.github.com>
2024-06-10 11:53:32 +0530

    Update app.js

--- Js/app.js
+++ Js/app.js
@@ -1,1 +1,21 @@
 
+let plus = document.querySelectorAll('.plus');
+let cross = document.querySelectorAll('.cross');
+let hidden = document.querySelectorAll('.hidden');
+let para = document.querySelectorAll('.para');
+
+for(let i = 0; i< plus.length;i++){
+    plus[i].addEventListener('click',()=>{
+        plus[i].classList.toggle('hidden');
+        cross[i].classList.toggle('hidden');
+        para[i].classList.toggle('hidden');
+    });
+}
+
+for(let i=0; i <cross.length;i++){
+    cross[i].addEventListener('click', function(){
+        cross[i].classList.toggle('hidden');
+        plus[i].classList.toggle('hidden');
+        para[i].classList.toggle('hidden');
+    })
+}
